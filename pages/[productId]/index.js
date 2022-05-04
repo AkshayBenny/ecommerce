63dadc026fa226e10746c90ef09d13686c4eab31
Akshay Benny <akshay@pop-os.localdomain>
2022-05-05 01:35:32 +0530

    bug fix

--- pages/[productId]/index.js
+++ pages/[productId]/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
-const Product = ({ product }) => {
+const Product = (product) => {
+  console.log(product);
   return <div>Product page</div>;
 };
 
 export default Product;
 
-export async function getStaticProps() {
-  const response = await fetch('https://fakestoreapi.com/products');
-  const data = await response.json();
+export async function getStaticProps(context) {
+  const productId = context.params.productId;
+  const response = await fetch(
+    `https://fakestoreapi.com/products?id=${productId}`
+  );
+  const productData = await response.json();
 
   return {
     props: {
-      product: data,
+      product: productData,
     },
   };
 }
