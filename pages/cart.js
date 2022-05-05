95732eaaa44ca13b628c4e6b441467d54a9fa9fd
Akshay Benny <akshay@pop-os.localdomain>
2022-05-05 21:19:46 +0530

    edit

--- pages/cart.js
+++ pages/cart.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const cart = ({ cart }) => {
-  console.log(cart);
-  return <div>{
-      cart.products.map((product,index)=> {
-          return <div key={index}></div>
-      })
-}</div>;
+const cart = ({ cart, products }) => {
+  console.log(cart.products);
+  console.log(products);
+
+  return (
+    <div>
+      {cart.products.map((product, index) => {
+        return <div key={index}></div>;
+      })}
+    </div>
+  );
 };
 
 export default cart;
@@ -15,10 +19,28 @@ export async function getServerSideProps() {
   const userId = 2;
   const response = await fetch('https://fakestoreapi.com/carts');
   const data = await response.json();
+
   const userCart = data.find((item) => item.userId == userId);
+
+  const userProductsArray = [];
+
+  const myProductFetcher = async (pid) => {
+    const productResponse = await fetch('https://fakestoreapi.com/products');
+    const productData = await productResponse.json();
+    const productId = pid;
+    const myProduct = productData.find((item) => item.id == productId);
+    userProductsArray.push(myProduct);
+  };
+
+  //get the products correspnding to the user
+  //   const userProducts = await userCart.products.map((item) => {
+  //     myProductFetcher(item.productId);
+  //   });
+
   return {
     props: {
       cart: userCart,
+      products: typeof userCart.products,
     },
   };
 }
